482ba9bec2abcf9e58a2951c3dfb2d2726721905
Niclas Becker <enbecko1996@googlemail.com>
2017-02-20 16:20:17 +0100

    New ideas

--- src/main/java/com/enbecko/modcreator/Main_ModCreator.java
+++ src/main/java/com/enbecko/modcreator/Main_ModCreator.java
@@ -10,6 +10,8 @@ public class Main_ModCreator
 {
     public static final String MODID = "modcreator";
     public static final String VERSION = "1.0";
+
+    public static int maxBlockSize = 16;
     
     @EventHandler
     public void init(FMLInitializationEvent event)
